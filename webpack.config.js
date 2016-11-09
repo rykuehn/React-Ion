8550de874c1e4ee233ee61ba37d34257555c203e
Andrew <anxsaiyuki@gmail.com>
2016-11-09 10:07:28 -0800

    Added react routes to the web app

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 module.exports = {
   cache: true,
-  entry: './src/js/index.jsx',
+  entry: './src/js/App.jsx',
   output: {
     path: path.join(__dirname, 'dist'),
     publicPath: '',
