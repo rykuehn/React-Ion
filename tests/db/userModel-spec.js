3ebb95ede6f62db6cf8519a214512ceb3ff11cfc
Cheney Mao <cheneymao94@gmail.com>
2016-11-11 22:29:05 -0800

    (test) Add tests for user creation

--- tests/db/userModel-spec.js
+++ tests/db/userModel-spec.js
@@ -4,27 +4,67 @@ const User = require('../../db/models/userModel.js');
 
 const describe = mocha.describe;
 const it = mocha.it;
-const before = mocha.before;
+const beforeEach = mocha.beforeEach;
 const after = mocha.after;
 
 describe('User Model', () => {
   const username = 'gold';
   const password = 'hahaha';
   const salt = 'somethingRandom';
-  const user = { username, password, salt };
+  const newUser = { username, password, salt };
+
+  beforeEach((done) => {
+    User.remove(username, (err) => {
+      if (err) { console.error(err); }
+      done();
+    });
+  });
 
   after((done) => {
-    User.remove({ username }, (err) => {
-      if (err) { console.err(err); }
+    User.remove(username, (err) => {
+      if (err) { console.error(err); }
       done();
     });
   });
 
   describe('User creation: ', () => {
-    it('Invalidates invalid users', (done) => {
+    it('Does not add invalid users to database', (done) => {
+      User.create({ username: '123' }, (err) => {
+        expect(err).to.exist;
+        done();
+      });
     });
 
-    it('Validates valid users', (done) => {
+    it('Adds valid users to database', (done) => {
+      User.create(newUser, (err) => {
+        expect(err).to.not.exist;
+        User.get((err, users) => {
+          expect(err).to.not.exist;
+          expect(users.length).equal(1);
+          expect(users[0].username).to.equal('gold');
+          done();
+        });
+      });
     });
   });
+
+  describe('User Update: ', () => {
+    it('Does not add or remove users from database', (done) => {
+      done();
+    });
+
+    it('Updates existing users from database', (done) => {
+      done();
+    });
+  });
+
+  // describe('User creation: ', () => {
+  //   it('Does not add invalid users to database', (done) => {
+  //     done();
+  //   });
+
+  //   it('Adds valid users to database', (done) => {
+  //     done();
+  //   });
+  // });
 });
