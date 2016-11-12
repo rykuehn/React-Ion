6c221b94c198400fe3d68c9a2e6bfd7a3675b041
Cheney Mao <cheneymao94@gmail.com>
2016-11-11 17:17:50 -0800

    (feat) Update database schema

--- db/schema.sql
+++ db/schema.sql
@@ -3,23 +3,31 @@ CREATE DATABASE reaction;
 USE reaction;
 
 CREATE TABLE users (
-  id int NOT NULL AUTO_INCREMENT,
+  id        int         NOT NULL AUTO_INCREMENT,
   user_name varchar(40) NOT NULL,
   PRIMARY KEY (ID),
   UNIQUE KEY (user_name)
 );
 
-CREATE TABLE outlines (
-  id int NOT NULL AUTO_INCREMENT,
-  value varchar(1000)  NOT NULL,
+CREATE TABLE projects (
+  id   int            NOT NULL AUTO_INCREMENT,
+  tree varchar(1000)  NOT NULL,
   PRIMARY KEY (ID)
 );
 
-CREATE TABLE user_config (
-  id int NOT NULL AUTO_INCREMENT,
-  user_id int NOT NULL,
-  outline_id int NOT NULL,
+CREATE TABLE permissions (
+  id   int         NOT NULL AUTO_INCREMENT,
+  name varchar(40) NOT NULL,
+  PRIMARY KEY (ID)
+);
+
+CREATE TABLE user_project (
+  id            int NOT NULL AUTO_INCREMENT,
+  user_id       int NOT NULL,
+  project_id    int NOT NULL,
+  permission_id int NOT NULL,
   PRIMARY KEY (ID),
-  FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE ON UPDATE CASCADE,
-  FOREIGN KEY (outline_id) REFERENCES outlines (id) ON DELETE CASCADE ON UPDATE CASCADE
+  FOREIGN KEY (user_id)       REFERENCES users (id)       ON DELETE CASCADE ON UPDATE CASCADE,
+  FOREIGN KEY (project_id)    REFERENCES projects (id)    ON DELETE CASCADE ON UPDATE CASCADE,
+  FOREIGN KEY (permission_id) REFERENCES permissions (id) ON DELETE CASCADE ON UPDATE CASCADE
 );
