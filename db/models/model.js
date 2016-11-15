bd489db7f9f7fb004faa0005a6cfa54c434ffe8a
Cheney Mao <cheneymao94@gmail.com>
2016-11-14 20:13:07 -0800

    (refactor) Move common model methods to model class

---
+++ db/models/model.js
@@ -0,0 +1,70 @@
+const db = require('../../server/config/connection');
+require('airbnb-js-shims');
+const _ = require('lodash');
+
+const Model = class Model {
+  constructor(modelName) {
+    this.modelName = modelName;
+  }
+
+  find(params, cb) {
+    const keys = Object.keys(params);
+    const vals = Object.values(params).map(a => a.toString());
+    let queryString = `select * from ${this.modelName}`;
+
+    if (keys.length > 0) {
+      queryString += ` where ${keys.map(a => `${a}=?`).join(' and ')}`;
+      db.query(queryString, vals, (err, results) => {
+        if (cb) { cb(err, results); }
+      });
+    } else {
+      db.query(queryString, (err, results) => {
+        if (cb) { cb(err, results); }
+      });
+    }
+  }
+
+  create(props, cb) {
+    const keys = Object.keys(props);
+    const vals = Object.values(props);
+    const propString = `(${keys.join(',')})`;
+    const valString = `(${_.range(keys.length).map(() => '?').join(',')})`;
+    const queryString = `insert into ${this.modelName}${propString}
+                         value ${valString}`;
+    db.query(queryString, vals, (err, results) => {
+      if (cb) { cb(err, results); }
+    });
+  }
+
+  update(query, props, cb) {
+    const pkeys = Object.keys(props);
+    const pvals = Object.values(props);
+    const qkeys = Object.keys(query);
+    const qvals = Object.values(query);
+    const queryString = `update ${this.modelName} set ${pkeys.map(a => `${a}=?`).join(', ')}
+                         where ${qkeys.map(a => `${a}=?`).join(' and ')}`;
+    db.query(queryString, pvals.concat(qvals), (err, results) => {
+      if (cb) { cb(err, results); }
+    });
+  }
+
+  remove(params, cb) {
+    const keys = Object.keys(params);
+    const vals = Object.values(params).map(a => a.toString());
+    let queryString = `delete from ${this.modelName}`;
+
+    if (keys.length > 0) {
+      queryString += ` where ${keys.map(a => `${a}=?`).join(' and ')}`;
+      db.query(queryString, vals, (err, results) => {
+        if (cb) { cb(err, results); }
+      });
+    } else {
+      db.query(queryString, (err, results) => {
+        if (cb) { cb(err, results); }
+      });
+    }
+  }
+};
+
+
+module.exports = Model;
--- tests/db/userModel-spec.js
+++ tests/db/userModel-spec.js
@@ -68,7 +68,7 @@ describe('User Model', () => {
     it('Adds valid users to database', (done) => {
       User.create(newUser, (err) => {
         expect(err).to.not.exist;
-        User.fetch(newUser, (err2, users) => {
+        User.find(newUser, (err2, users) => {
           expect(err2).to.not.exist;
           expect(users.length).to.not.equal(0);
           expect(users[0].username).to.equal('neverused');
@@ -82,9 +82,9 @@ describe('User Model', () => {
     it('Does not add or remove users from database', (done) => {
       User.create(newUser, (err) => {
         expect(err).to.not.exist;
-        User.update(newUser2, (err2) => {
+        User.update(newUser, newUser2, (err2) => {
           expect(err2).to.not.exist;
-          User.fetch({ username: 'neverused' }, (err3, users) => {
+          User.find({ username: 'neverused' }, (err3, users) => {
             expect(err3).to.not.exist;
             expect(users.length).to.not.equal(0);
             done();
@@ -96,9 +96,9 @@ describe('User Model', () => {
     it('Updates existing users from database', (done) => {
       User.create(newUser, (err) => {
         expect(err).to.not.exist;
-        User.update(newUser2, (err2) => {
+        User.update(newUser, newUser2, (err2) => {
           expect(err2).to.not.exist;
-          User.fetch({ username: 'neverused' }, (err3, users) => {
+          User.find({ username: 'neverused' }, (err3, users) => {
             expect(err3).to.not.exist;
             expect(users[0].password).to.equal('watever2');
             done();
@@ -108,15 +108,15 @@ describe('User Model', () => {
     });
   });
 
-  describe('User fetch: ', () => {
-    it('fetch all users if passed empty object', (done) => {
+  describe('User find: ', () => {
+    it('Find all users if passed empty object', (done) => {
       User.create(newUser, (err) => {
         expect(err).to.not.exist;
         User.create(newUser3, (err2) => {
           expect(err2).to.not.exist;
           User.create(newUser4, (err3) => {
             expect(err3).to.not.exist;
-            User.fetch({}, (err4, users) => {
+            User.find({}, (err4, users) => {
               expect(err4).to.not.exist;
               expect(users.length).to.be.above(2);
               done();
@@ -133,7 +133,7 @@ describe('User Model', () => {
           expect(err2).to.not.exist;
           User.create(newUser4, (err3) => {
             expect(err3).to.not.exist;
-            User.fetch({ username: 'hahahaha' }, (err4, users) => {
+            User.find({ username: 'hahahaha' }, (err4, users) => {
               expect(err4).to.not.exist;
               expect(users.length).to.equal(1);
               expect(users[0].password).to.equal('watever3');
@@ -153,7 +153,7 @@ describe('User Model', () => {
           expect(err2).to.not.exist;
           User.remove({ username: 'neverused' }, (err3) => {
             expect(err3).to.not.exist;
-            User.fetch({ username: 'neverused' }, (err4, users) => {
+            User.find({ username: 'neverused' }, (err4, users) => {
               expect(err4).to.not.exist;
               expect(users.length).to.equal(0);
               done();
