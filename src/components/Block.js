e0cb8af3dd8c17bc87223853faa686b1b030388a
Robin Kuehn <kuehn.robin@gmail.com>
2016-11-14 19:32:38 -0800

    resolved

--- src/components/Block.js
+++ src/components/Block.js
@@ -3,7 +3,6 @@ import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import { setSelected } from '../actions/selected';
 
-<<<<<<< HEAD
 const Block = ({
   backgroundColor,
   height,
