02629d25f95c7e2da56211f159aa02fa73d7afd0
Andrew <anxsaiyuki@gmail.com>
2016-11-15 14:18:57 -0800

    (refactor) user folder will be removed after sending

--- server/utils/zip.js
+++ server/utils/zip.js
@@ -1,10 +1,21 @@
 const archiver = require('archiver');
 const path = require('path');
+const helper = require('./generateHelper');
 
 module.exports = (res, userId) => {
   const archive = archiver('zip');
+  console.log('zipping');
+
   archive.on('err', (err) => { throw err; });
+
+  archive.on('end', () => {
+    console.log('Archive wrote %d bytes', archive.pointer());
+    helper.removeUserFolder(userId, () => {});
+  });
+
+  res.attachment('project.zip');
+
   archive.pipe(res);
-  archive.directory(path.join(__dirname, '../../user/' + userId), 'app/');
+  archive.directory(path.join(__dirname, `../../user/${userId}`), 'app/');
   archive.finalize();
 };
