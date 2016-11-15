cadeebe6862a7c761823a6cecbc76f855a0509b6
Andrew <anxsaiyuki@gmail.com>
2016-11-15 15:59:39 -0800

    (refactor) Moved the code for child from blockcomponent to helper.

--- server/utils/zip.js
+++ server/utils/zip.js
@@ -10,7 +10,7 @@ module.exports = (res, userId) => {
 
   archive.on('end', () => {
     console.log('Archive wrote %d bytes', archive.pointer());
-    helper.removeUserFolder(userId, () => {});
+    //helper.removeUserFolder(userId, () => {});
   });
 
   res.attachment('project.zip');
