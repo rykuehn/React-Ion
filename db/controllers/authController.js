dabeff6c169b0d2069d4f4d63ba4101556425deb
Cheney Mao <cheneymao94@gmail.com>
2016-11-16 10:27:46 -0800

    (feat) Complete authentication routes and add tests

--- db/controllers/authController.js
+++ db/controllers/authController.js
@@ -1,35 +1,35 @@
 const User = require('../models/userModel');
-const bcrypt = require('bcrypt-nodejs');
 
 module.exports.login = (req, res) => {
   if (req.user) {
-    return res.json(req.user);
+    const user = req.user;
+    return res.json({
+      id: user.id,
+      username: user.username,
+    });
   }
   return res.status(404).end('Failed to authenticate user');
 };
 
 module.exports.signup = (req, res) => {
   const username = req.body.username;
-  const password = req.body.password;
 
   User.findOne({ username }, (err, user) => {
     if (err) { return res.status(404).end('Unable to retrieve user'); }
     if (user) { return res.status(404).end('Username taken'); }
-    return bcrypt.genSalt(10, (err2, salt) => {
-      if (err2) { return res.status(404).end('Unable to create salt'); }
-      return bcrypt.hash(password, salt, null, (err3, hash) => {
-        if (err3) { return res.status(404).end('Unable to hash password'); }
-        return User.create({ username, password: hash, salt }, (err4, newUser) => {
-          if (err4) { return res.status(404).end('Unable to create user'); }
-          return res.json(newUser);
-        });
+    return User.create(req.body, (err2, newUser) => {
+      if (err2) { res.status(404).end('Unable to create user'); }
+      return res.json({
+        id: newUser.id,
+        username: newUser.username,
       });
     });
   });
 };
 
 module.exports.logout = (req, res) => {
-  req.session.destroy(() => {
-    res.end('Goodbye');
+  req.session.destroy((err) => {
+    if (err) { return res.status(404).send('Failed to destroy session'); }
+    return res.status(200).send('Logout successful');
   });
 };
--- tests/server/authRoutes-spec.js
+++ tests/server/authRoutes-spec.js
@@ -4,7 +4,7 @@ const mocha = require('mocha');
 const expect = require('chai').expect;
 const request = require('request');
 const User = require('../../db/models/userModel.js');
-const Project = require('../../db/models/projectModel.js');
+// const Project = require('../../db/models/projectModel.js');
 
 const port = process.env.PORT || 8080;
 const host = `http://localhost:${port}`;
@@ -16,18 +16,21 @@ const after = mocha.after;
 
 describe('Auth Routes', () => {
   before((done) => {
-    const options = {
-      method: 'GET',
-      uri: `${host}/api/project/logout`,
-    };
-    request(options, (err) => {
-      if (err) { console.error(err); }
-      User.create({
-        username: 'Cheney',
-        password: 'notsafe',
-      }, (err2) => {
-        expect(err2).to.not.exist;
-        done();
+    User.remove({}, (err) => {
+      expect(err).to.not.exist;
+      const options = {
+        method: 'GET',
+        uri: `${host}/api/project/logout`,
+      };
+      request(options, (err2) => {
+        if (err2) { console.error(err2); }
+        User.create({
+          username: 'Cheney',
+          password: 'notsafe',
+        }, (err3) => {
+          expect(err3).to.not.exist;
+          done();
+        });
       });
     });
   });
@@ -41,7 +44,7 @@ describe('Auth Routes', () => {
 
   describe('POST /login ', () => {
     const requestWithSession = request.defaults({ jar: true });
-    it('Logs user in and creates session', (done) => {
+    it('Rejects user with wrong password', (done) => {
       const options = {
         method: 'POST',
         followAllRedirects: true,
@@ -53,7 +56,9 @@ describe('Auth Routes', () => {
       };
 
       requestWithSession(options, (err2, res, body) => {
+        expect(err2).to.not.exist;
         expect(body).to.equal('Unauthorized');
+        expect(res.statusCode).to.equal(401);
         done();
       });
     });
@@ -71,16 +76,48 @@ describe('Auth Routes', () => {
 
       requestWithSession(options, (err2, res, body) => {
         expect(err2).to.not.exist;
-        console.log(body);
-        expect(body).to.not.equal('Failed to authenticate user');
+        expect(res.statusCode).to.equal(200);
+        expect(body.username).to.equal('Cheney');
         done();
       });
     });
   });
 
   describe('POST /signup ', () => {
-    it('Signs user up and creates session', (done) => {
-      done();
+    const requestWithSession = request.defaults({ jar: true });
+    it('Signs new user up and creates session', (done) => {
+      const options = {
+        method: 'POST',
+        followAllRedirects: true,
+        uri: `${host}/signup`,
+        json: {
+          username: 'Cheney1',
+          password: 'notsafe',
+        },
+      };
+      requestWithSession(options, (err2, res, body) => {
+        expect(err2).to.not.exist;
+        expect(res.statusCode).to.equal(200);
+        expect(body.username).to.equal('Cheney1');
+        done();
+      });
+    });
+
+    it('Returns 404 when user already exists', (done) => {
+      const options = {
+        method: 'POST',
+        followAllRedirects: true,
+        uri: `${host}/signup`,
+        json: {
+          username: 'Cheney1',
+          password: 'asd',
+        },
+      };
+      requestWithSession(options, (err2, res) => {
+        expect(err2).to.not.exist;
+        expect(res.statusCode).to.equal(404);
+        done();
+      });
     });
   });
 
@@ -88,14 +125,13 @@ describe('Auth Routes', () => {
     it('Signs user out and destroys session', (done) => {
       const options = {
         method: 'GET',
-        uri: `${host}/api/project/logout`,
+        uri: `${host}/logout`,
       };
-      User.remove({}, (err) => {
-        expect(err).to.not.exist;
-        request(options, (err2) => {
-          expect(err2).to.not.exist;
-          done();
-        });
+      request(options, (err2, res, body) => {
+        expect(err2).to.not.exist;
+        expect(res.statusCode).to.equal(200);
+        expect(body).to.equal('Logout successful');
+        done();
       });
     });
   });
