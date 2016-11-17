2be821885fc7514589d0393ddd1b5f521ef7af34
Cheney Mao <cheneymao94@gmail.com>
2016-11-17 12:18:00 -0800

    (feat) Add cross origin headers

--- server/config/routes.js
+++ server/config/routes.js
@@ -3,7 +3,13 @@ const projectRouter = require('./routers/projectRouter');
 const authRouter = require('./routers/authRouter');
 
 module.exports = (app) => {
+  app.all('/', (req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'X-Requested-With');
+    next();
+  });
   app.use('/api/user', userRouter);
   app.use('/api/project', projectRouter);
   app.use('/', authRouter);
+  app.get('/*', (req, res) => { res.status(404).send('404 Invalid Request'); });
 };
