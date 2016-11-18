5d71c343bdd5f9345135b99ccac1958ec6610c57
Cheney Mao <cheneymao94@gmail.com>
2016-11-17 16:17:12 -0800

    (bug) Remove console logs for user serialization/deserialization

--- server/config/auth.js
+++ server/config/auth.js
@@ -6,12 +6,10 @@ const LocalStrategy = require('passport-local').Strategy;
 
 module.exports = () => {
   passport.serializeUser((user, done) => {
-    console.log('serialized', user);
     done(null, user.id);
   });
 
   passport.deserializeUser((id, done) => {
-    console.log('deserialized', id);
     User.findOne({ id }, (err, user) => {
       done(err, user);
     });
