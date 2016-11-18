b3e98c10f93d08db7fec20f1e9ce106549197311
Matthew Vincent <matthewlawrencevincent@gmail.com>
2016-11-17 16:10:32 -0800

    refactored sliders

--- src/components/FlexSlider.js
+++ src/components/FlexSlider.js
@@ -1,48 +1,29 @@
 import React from 'react';
+import Slider from './Slider';
 
-export default class FlexSlider extends React.Component {
-  componentDidMount() {
-    this.updateFlex();
-  }
-
-  updateFlex() {
-    const context = this;
-    const flex = this.props.info.props.flex;
-
-    if (flex) {
-      setTimeout(() => {
-        context.flex.value = flex;
-      });
+const FlexSlider = ({
+  updateProps,
+  selected,
+  info,
+}) => (
+  <div
+    className={selected === 0
+      ? 'hidden'
+      : 'slider'
     }
-  }
+  >
+    <Slider
+      min={0}
+      max={10}
+      step={1}
+      unit={null}
+      propName={'flex'}
+      selected={selected}
+      updateProps={updateProps}
+      title={'FLEX'}
+      initialValue={info.props.flex}
+    />
+  </div>
+);
 
-  render() {
-    const { updateProps, selected } = this.props;
-    this.updateFlex();
-    return (
-      <div
-        className={selected === 0
-          ? 'hidden'
-          : 'slider'
-        }
-      > FLEX
-        <input
-          type="range"
-          min={0}
-          max={10}
-          ref={i => this.flex = i}
-          onChange={() => updateProps(
-            'flex', this.flex.value, selected, 'onChange',
-          )}
-          onMouseDown={() => updateProps(
-            'flex', this.flex.value, selected, 'onMouseDown',
-          )}
-          onMouseUp={() => updateProps(
-            'flex', this.flex.value, selected, 'onMouseUp',
-          )}
-
-        />
-      </div>
-    );
-  }
-}
+export default FlexSlider;
--- src/components/HeightSlider.js
+++ src/components/HeightSlider.js
@@ -1,57 +1,35 @@
 import React from 'react';
+import Slider from './Slider';
 
-export default class HeightSlider extends React.Component {
+const HeightSlider = ({
+  updateProps,
+  selected,
+  info,
+}) => {
+  const direction = info.parent
+    ? info.parent.props.flexDirection
+    : null;
 
-  componentDidMount() {
-    this.updateHeight();
-  }
+  return (
+    <div
+      className={direction === 'column'
+        ? 'hidden'
+        : 'slider'
+      }
+    >
+      <Slider
+        min={0}
+        max={selected === 0 ? 10000 : 100}
+        step={selected === 0 ? 100 : 10}
+        unit={selected === 0 ? 'px' : '%'}
+        propName={'height'}
+        selected={selected}
+        updateProps={updateProps}
+        title={'HEIGHT'}
+        initialValue={info.props.height[0]}
+      />
+    </div>
+  );
+};
 
-  updateHeight() {
-    const context = this;
-    const height = this.props.info.props.height;
-    if (height) {
-      setTimeout(() => {
-        context.height.value = height[0];
-      });
-    }
-  }
-
-  render() {
-    const { updateProps, selected, info } = this.props;
-    const direction = info.parent ? info.parent.props.flexDirection : null;
-
-    this.updateHeight();
-    return (
-      <div
-        className={direction === 'column'
-          ? 'hidden'
-          : 'slider'
-        }
-      > HEIGHT
-        <input
-          type="range"
-          min={0}
-          max={selected === 0 ? 10000 : 100}
-          step={selected === 0 ? 100 : 10}
-          ref={i => this.height = i}
-          onChange={() => {
-            updateProps(
-              'height',
-              [this.height.value, selected === 0 ? 'px' : '%'],
-              selected,
-              'onChange',
-            );
-          }}
-          onMouseUp={() => {
-            updateProps(
-              'height',
-              [this.height.value, '%'],
-              selected,
-              'onMouseUp',
-            );
-          }}
-        />
-      </div>
-    );
-  }
-}
+export default HeightSlider;
