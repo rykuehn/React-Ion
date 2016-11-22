e1b3e53de9e41cffa1da4d31faa1f585427b5bd2
Matthew Vincent <matthewlawrencevincent@gmail.com>
2016-11-22 12:26:45 -0800

    added login/signup page

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,22 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-// import { Router, Route, browserHistory } from 'react-router';
 import { Provider } from 'react-redux';
 import store from '../store/store';
 import Editor from '../containers/tool_component/Editor';
-// import NotFound from './NotFound';
-require('../scss/index.scss');
-// const App = () => (
-//   <Provider store={store}>
-//     <Router history={browserHistory}>
-//       <Route path="/" component={Index} />
-//       <Route path="/Home" component={Home} />
-//       <Route path="*" component={NotFound} />
-//     </Router>
-//   </Provider>
-// );
-
-// const Home = () => <h1>Hello from Home!</h1>;
+import '../scss/index.scss';
 
 const App = () => (
   <Provider store={store}>
