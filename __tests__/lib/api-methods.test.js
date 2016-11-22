80856daae00464424fe072a653c1824b55088fbc
Cheney Mao <cheneymao94@gmail.com>
2016-11-21 20:08:35 -0800

    (test) Set up jest

---
+++ __tests__/lib/api-methods.test.js
@@ -0,0 +1,64 @@
+/* eslint-disable no-undef */
+import { expect } from 'chai';
+import {
+  getUserProjects,
+  login,
+  signup,
+  logout,
+  getProject,
+  getAllProjects,
+  removeProject,
+  saveProject,
+  updateProject,
+} from '../../src/lib/api-methods';
+
+describe('API Methods', () => {
+  describe('Sign Up', () => {
+    it('signs user up and creates session for user', (done) => {
+      signup('cheney123', '123')
+        .then((userInfo) => {
+          console.log(userInfo);
+          done();
+        })
+        .catch((err) => {
+          console.log(err);
+          done();
+        });
+    });
+  });
+
+  // describe('Login', () => {
+  //   it('renders an ".toolbar"', () => {
+  //   });
+  // });
+
+  // describe('Logout', () => {
+  //   it('renders an ".toolbar"', () => {
+  //   });
+  // });
+
+  // describe('Get Project', () => {
+  //   it('renders an ".toolbar"', () => {
+  //   });
+  // });
+
+  // describe('Get All Projects', () => {
+  //   it('renders an ".toolbar"', () => {
+  //   });
+  // });
+
+  // describe('Remove Project', () => {
+  //   it('renders an ".toolbar"', () => {
+  //   });
+  // });
+
+  // describe('Save Project', () => {
+  //   it('renders an ".toolbar"', () => {
+  //   });
+  // });
+
+  // describe('Update Project', () => {
+  //   it('renders an ".toolbar"', () => {
+  //   });
+  // });
+});
