530f68a05fd5f892d9744a4038f08dbcfd58ba8f
Cheney Mao <cheneymao94@gmail.com>
2016-11-27 03:57:16 -0800

    (refactor) Update port number to 8080 and change host url

--- server/server.js
+++ server/server.js
@@ -5,7 +5,7 @@ const app = express();
 require('./config/middleware')(app, express);
 require('./config/routes')(app, express);
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8080;
 
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
